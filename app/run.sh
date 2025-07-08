d603377a707e77ca6109541e78916b5935ceca4f
Vladimir Kimbris <kimbris.vladimir@wb.ru>
2025-07-08 19:25:56 +0300

    change ports

--- app/run.sh
+++ app/run.sh
@@ -5,4 +5,4 @@ aws s3 cp s3://masking/data/names.txt   /app/data/names.txt   --endpoint-url "$A
 aws s3 cp s3://masking/data/surnames.txt /app/data/surnames.txt --endpoint-url "$AWS_ENDPOINTURL"
 aws s3 cp s3://masking/data/midnames.txt /app/data/midnames.txt --endpoint-url "$AWS_ENDPOINTURL"
 
-uvicorn main:app --host 0.0.0.0 --port 8080
+uvicorn main:app --host 0.0.0.0 --port 8000
